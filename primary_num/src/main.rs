cd2cc4c39de4df6a0273267bf7c88fcf7d4e1bc8
Suvineet Singh <suvineet1998@gmail.com>
2024-03-26 11:59:43 +0000

    Calculate without the use of Mutex and manual locking and unlocking.

--- primary_num/src/main.rs
+++ primary_num/src/main.rs
@@ -21,6 +21,42 @@ fn is_prime(n: usize) -> bool {
     }
 }
 
+fn main_without_mutex() {
+    let num_cpus = num_cpus::get();
+    println!("Using {num_cpus} threads.");
+    let mut candidates: Vec<usize> = (0..MAX_NUMBER).collect();
+
+    // Perform the calculation
+    let start = Instant::now(); // We are not timing the initial condition
+    let mut primes: Vec<usize> = Vec::with_capacity(10_000);
+
+    // Shuffle the candidates so we can randomise the slices for each thread.
+    candidates.shuffle(&mut rand::thread_rng());
+
+    std::thread::scope(|scope| {
+        let mut handles = Vec::with_capacity(num_cpus);
+        let chunks = candidates.chunks(num_cpus); // chunks at a time. So if 16 cpus, then 16 items per list.
+
+        for chunk in chunks {
+            let handle =
+                scope.spawn(move || chunk.iter().filter(|n| is_prime(**n)).map(|n| *n).collect());
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            let local_result: Vec<usize> = handle.join().unwrap();
+            primes.extend(local_result);
+        }
+    });
+
+    // Time how long it took
+    let elapsed = start.elapsed();
+
+    // Results
+    println!("Found {} primes", primes.len());
+    println!("Calculated in {:.4} seconds", elapsed.as_secs_f32());
+}
+
 fn main() {
     let num_cpus = num_cpus::get();
     println!("Using {num_cpus} threads.");
