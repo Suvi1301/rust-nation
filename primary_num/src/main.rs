014e887495f1df588e5e1315bf374e585b9eda2e
Suvineet Singh <suvineet1998@gmail.com>
2024-03-26 10:30:36 +0000

    Modify program to use Arc Mutex Vector and calculate multithreaded in chunks

--- primary_num/src/main.rs
+++ primary_num/src/main.rs
@@ -1,5 +1,9 @@
-use std::time::Instant;
+use std::{
+    sync::{Arc, Mutex},
+    time::Instant,
+};
 
+// const NUM_THREADS: usize = 10;
 const MAX_NUMBER: usize = 100_000;
 
 /// Really inefficient prime number calculator
@@ -17,18 +21,45 @@ fn is_prime(n: usize) -> bool {
 }
 
 fn main() {
+    let num_cpus = num_cpus::get();
+    println!("Using {num_cpus} threads.");
     let candidates: Vec<usize> = (0..MAX_NUMBER).collect();
 
     // Perform the calculation
     let start = Instant::now(); // We are not timing the initial condition
-    let primes: Vec<usize> = candidates
-        .iter()
-        .filter(|n| is_prime(**n))
-        .map(|n| *n)
-        .collect();
+    let primes: Arc<Mutex<Vec<usize>>> = Arc::new(Mutex::new(Vec::new()));
+    // let primes: Vec<usize> = candidates
+    //     .iter()
+    //     .filter(|n| is_prime(**n))
+    //     .map(|n| *n)
+    //     .collect();
+
+    std::thread::scope(|scope| {
+        let chunks = candidates.chunks(candidates.len() / num_cpus);
+
+        for chunk in chunks {
+            let my_primes = primes.clone();
+            scope.spawn(move || {
+                // Perform the same filter/map/collect chain as we did single-threaded
+                let local_results: Vec<usize> =
+                    chunk.iter().filter(|n| is_prime(**n)).map(|n| *n).collect();
+
+                // Lock the shared results list
+                let mut lock = my_primes.lock().unwrap();
+
+                // Extend the results with our discovered primes
+                lock.extend(local_results);
+            });
+        }
+
+        // The scope will automatically wait for child threads to finish here.
+    });
+
+    // Time how long it took
     let elapsed = start.elapsed();
 
     // Results
-    println!("Found {} primes", primes.len());
+    let lock = primes.lock().unwrap();
+    println!("Found {} primes", lock.len());
     println!("Calculated in {:.4} seconds", elapsed.as_secs_f32());
 }
